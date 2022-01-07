b6faf41c6283a794f0f01cc64d8e5c333000958e
amirbahador.pv <amirbahador.pv@gmail.com>
2022-01-07 15:28:25 -0500

    data type

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "GO Conference"
+	conferenceName :="GO Conference"
 	const conferenceTickets = 50        // Not Changeable
-	var remainingTickets = 50
+	var remainingTickets uint= 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
+
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	// ask user for their name
 
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets \n", userName, userTickets)
 }
 
