157164c62f31ec685a84b309e319d0363a472649
amirbahador.pv <amirbahador.pv@gmail.com>
2022-01-08 08:19:31 -0500

    Slices

--- main.go
+++ main.go
@@ -1,24 +1,23 @@
 package main
+
 import "fmt"
 
 func main() {
-	conferenceName :="GO Conference"
-	const conferenceTickets = 50        // Not Changeable
-	var remainingTickets uint= 50
-	var bookings [50]string	
+	conferenceName := "GO Conference"
+	const conferenceTickets = 50 // Not Changeable
+	var remainingTickets uint = 50
+	bookings := []string{} // now this is an slice
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets %T, conferenceName is %T \n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
 
-	// ask user for their name
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
 
@@ -30,27 +29,11 @@ func main() {
 
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
-	// fmt.Println(remainingTickets)
-	// fmt.Println(&remainingTickets) // Pointer = memory location
 
 	remainingTickets = remainingTickets - userTickets
-    bookings[0] = firstName + " " + lastName
-	fmt.Printf("The whole array: %v \n", bookings)
-	fmt.Printf("The first value: %v \n", bookings[0])
-	fmt.Printf("Array type: %T \n", bookings)
-	fmt.Printf("Array lenght: %v \n", len(bookings))
-
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v \n ", remainingTickets, conferenceTickets)
+	fmt.Printf("These are all our bookings: %v \n ", bookings)
 }
-
-
-
-
-
-
-
-
-
-
